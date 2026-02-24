c61008ae7ee4b714c09b956a960d8fd8c34ef642
CazzieLund <cazandra.lund.java25@edu.edugrade.se>
2026-02-24 10:27:06 +0100

    feat/create board entity

--- src/main/java/com/example/kanban/model/Board.java
+++ src/main/java/com/example/kanban/model/Board.java
@@ -1,5 +1,32 @@
 package com.example.kanban.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
+
+@Entity
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
     
 }
