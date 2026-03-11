4a1396f26293a737389e4497e1ce20d34956f222
CazzieLund <cazandra.lund.java25@edu.edugrade.se>
2026-03-11 01:11:26 +0100

    feat: create columnController

---
+++ src/main/java/com/example/kanban/controller/ColumnController.java
@@ -0,0 +1,37 @@
+package com.example.kanban.controller;
+
+import java.util.List;
+
+import com.example.kanban.dto.column.ColumnResponse;
+import com.example.kanban.dto.column.CreateColumnRequest;
+import com.example.kanban.dto.column.UpdateColumnRequest;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.kanban.service.ColumnService;
+
+@RestController
+@RequestMapping("api/column")
+public class ColumnController {
+    
+    public final ColumnService columnService;
+
+    public ColumnController(ColumnService columnService) {
+        this.columnService = columnService;
+    }
+
+    @GetMapping
+    public List<ColumnResponse> getAllColumns() {
+        return columnService.getAllColumns();
+    }
+
+    @PostMapping
+    public ColumnResponse createColumn(@RequestBody CreateColumnRequest request) {
+        return columnService.createColumn(request);
+    }
+
+    @PutMapping("/{id}")
+    public ColumnResponse updateColumn(Long id, @RequestBody UpdateColumnRequest request) {
+        return columnService.updateColumn(id, request);
+    }
+}
--- src/main/java/com/example/kanban/service/ColumnService.java
+++ src/main/java/com/example/kanban/service/ColumnService.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.example.kanban.dto.board.BoardResponse;
+import com.example.kanban.dto.column.UpdateColumnRequest;
 import com.example.kanban.dto.column.ColumnResponse;
 import com.example.kanban.dto.column.CreateColumnRequest;
+import com.example.kanban.model.Board;
 import com.example.kanban.model.BoardColumn;
+import com.example.kanban.repository.BoardRepository;
 import com.example.kanban.repository.ColumnRepository;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -16,9 +20,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class ColumnService {
 
     private final ColumnRepository columnRepository;
+    private final BoardRepository boardRepository;
 
-    public ColumnService(ColumnRepository columnRepository){
+    public ColumnService(ColumnRepository columnRepository, BoardRepository boardRepository){
         this.columnRepository = columnRepository;
+        this.boardRepository = boardRepository;
     }
 
     @Transactional(readOnly = true)
@@ -37,8 +43,12 @@ public class ColumnService {
 
     public ColumnResponse createColumn(CreateColumnRequest request) {
 
+        Board board = boardRepository.findById(request.getBoardId())
+            .orElseThrow(() -> new RuntimeException("Board not found"));
+
         BoardColumn column = new BoardColumn();
         column.setName(request.getName());
+        column.setBoard(board);
 
         BoardColumn saved = columnRepository.save(column);
 
@@ -46,6 +56,21 @@ public class ColumnService {
             saved.getId(),
             saved.getName(),
             saved.getBoard().getId()
+        );
+    }
+
+        public ColumnResponse updateColumn(Long id, UpdateColumnRequest request) {
+        BoardColumn column = columnRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Column not found"));
+
+        column.setName(request.getName());
+
+        BoardColumn updated = columnRepository.save(column);
+
+        return new ColumnResponse(
+            updated.getId(),
+            updated.getName(),
+            updated.getBoard().getId()
         );
     }
     
