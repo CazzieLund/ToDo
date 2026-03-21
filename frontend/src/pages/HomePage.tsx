1b60d4dce68b11f7485aefe6d3cda83861ed1dc9
CazzieLund <cazandra.lund.java25@edu.edugrade.se>
2026-03-21 20:50:11 +0100

    git add . git commit -m "feat: initialize frontend with vite and basic structure" git push

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Kanban</h1>
+            <p>Frontend initialized with Vite + React + Typescript.</p>
+        </div>
+    );
+}
+
+export default HomePage;
