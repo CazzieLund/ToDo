8c417af7b0a71c3e82dd72937a7e7aea552a776e
CazzieLund <cazandra.lund.java25@edu.edugrade.se>
2026-03-22 22:57:47 +0100

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,25 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {getBoards} from "../services/boardService.ts";
 
+
 function HomePage() {
+
+    const [boards, setBoards] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
     useEffect(() => {
         const fetchBoards = async () => {
             try {
                 const data = await getBoards();
                 console.log("Boards from backend:", data);
+
+                setBoards(data);
             } catch (error) {
                 console.error("Error fetching boards:", error);
+                setError("Failed to load boards");
+            } finally {
+                setLoading(false);
             }
         };
 
@@ -19,6 +30,29 @@ function HomePage() {
         <div>
             <h1>Kanban</h1>
             <p>Frontend initialized with Vite + React + Typescript.</p>
+
+            {loading && <p>Loading boards...</p>}
+
+            {error && <p>{error}</p>}
+
+            {!loading && boards.length === 0 && (
+                <p>No boards yet</p>
+            )}
+
+            {boards.map((board: any) => (
+                <div
+                    key={board.id}
+                    style={{
+                        border: "1px solid #ccc",
+                        padding: "1rem",
+                        marginBottom: "1rem",
+                        borderRadius: "8px"
+                    }}
+                >
+                    <h3>{board.name}</h3>
+                    <p>{board.description || "No description"}</p>
+                </div>
+            ))}
         </div>
     );
 }
